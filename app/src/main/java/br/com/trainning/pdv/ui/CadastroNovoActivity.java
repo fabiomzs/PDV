2d2c499079d9dc5941ec925e61c52c21d292c9a6
fabiomzs <fabio@muniz.eti.br>
2016-03-05 12:31:32 -0300

    Commit 12:30

---
+++ app/src/main/java/br/com/trainning/pdv/ui/CadastroNovoActivity.java
@@ -0,0 +1,44 @@
+package br.com.trainning.pdv.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import br.com.trainning.pdv.R;
+import butterknife.Bind;
+
+public class CadastroNovoActivity extends BaseActivity {
+
+    @Bind(R.id.editTextDescricao)
+    EditText editTextDescricao;
+    @Bind(R.id.editTextUnidade)
+    EditText editTextUnidade;
+    @Bind(R.id.editTextPreco)
+    EditText editTextPreco;
+    @Bind(R.id.editTextCodigo)
+    EditText editTextCodigo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_novo);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Cadastro", editTextDescricao.getText().toString());
+                Log.d("Cadastro", editTextUnidade.getText().toString());
+                Log.d("Cadastro", editTextPreco.getText().toString());
+                Log.d("Cadastro", editTextCodigo.getText().toString());
+            }
+        });
+    }
+}
